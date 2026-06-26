8ddb517c6b58e0144099b389a6d82eeff5e377cb
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-06-26 11:18:12 +0500

    chge

--- app/core/supabase.py
+++ app/core/supabase.py
@@ -4,13 +4,10 @@ from supabase import create_client
 
 load_dotenv()
 
-
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 supabase = create_client(
     SUPABASE_URL,
-    SUPABASE_KEY,
     SUPABASE_SERVICE_ROLE_KEY
 )
