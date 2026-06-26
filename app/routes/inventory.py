958dd0269d78533bb14ce8c2d9d33f9cb94f29e2
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-06-26 17:33:20 +0500

    inventory management

--- app/routes/inventory.py
+++ app/routes/inventory.py
@@ -4,9 +4,10 @@ from typing import Annotated
 from app.core.roles import admin_required
 
 from app.services.inventory_service import (
-    low_stock_products
+    get_inventory,
+    low_stock_products,
+    update_stock
 )
-
 
 router = APIRouter(
     prefix="/inventory",
@@ -14,9 +15,27 @@ router = APIRouter(
 )
 
 
+@router.get("/")
+def inventory(
+    user: Annotated[dict, Depends(admin_required)]
+):
+    return get_inventory()
+
+
 @router.get("/low-stock")
 def low_stock(
     user: Annotated[dict, Depends(admin_required)]
 ):
-
     return low_stock_products()
+
+
+@router.patch("/{product_id}")
+def change_stock(
+    product_id: str,
+    quantity: int,
+    user: Annotated[dict, Depends(admin_required)]
+):
+    return update_stock(
+        product_id,
+        quantity
+    )
--- app/services/inventory_service.py
+++ app/services/inventory_service.py
@@ -1,16 +1,58 @@
 from app.core.supabase import supabase
 
 
+def get_inventory():
+
+    result = (
+        supabase
+        .table("products")
+        .select("*")
+        .order("name")
+        .execute()
+    )
+
+    return result.data
+
+
 def low_stock_products():
 
     result = (
         supabase
         .table("products")
         .select("*")
-        .lte(
-            "stock",
-            5
-        )
+        .lte("stock", 5)
+        .order("stock")
+        .execute()
+    )
+
+    return result.data
+
+
+def update_stock(product_id, quantity):
+
+    product = (
+        supabase
+        .table("products")
+        .select("stock")
+        .eq("id", product_id)
+        .single()
+        .execute()
+    )
+
+    current_stock = product.data["stock"]
+
+    new_stock = current_stock + quantity
+
+    if new_stock < 0:
+        new_stock = 0
+
+    result = (
+        supabase
+        .table("products")
+        .update({
+            "stock": new_stock
+        })
+        .eq("id", product_id)
         .execute()
     )
 
