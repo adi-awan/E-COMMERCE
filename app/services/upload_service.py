adc3ac8faae283c9be5b7ab0b7ee028befff220a
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-06-26 10:20:01 +0500

    upload_services changes

--- app/services/upload_service.py
+++ app/services/upload_service.py
@@ -1,22 +1,45 @@
+from fastapi import HTTPException
 from app.core.supabase import supabase
 
 
 def upload_image(file):
 
-    file_bytes = file.file.read()
+    try:
 
-    path = f"products/{file.filename}"
+        file_bytes = file.file.read()
 
-    supabase.storage.from_("product-images").upload(
-        path,
-        file_bytes,
-        {"content-type": file.content_type}
-    )
+        path = f"products/{file.filename}"
 
-    url = supabase.storage.from_(
-        "product-images"
-    ).get_public_url(path)
+        response = (
+            supabase
+            .storage
+            .from_("product-images")
+            .upload(
+                path,
+                file_bytes,
+                {
+                    "content-type": file.content_type,
+                    "upsert": "true"
+                }
+            )
+        )
 
-    return {
-        "image_url": url
-    }
+        url = (
+            supabase
+            .storage
+            .from_("product-images")
+            .get_public_url(path)
+        )
+
+        return {
+            "image_url": url
+        }
+
+    except Exception as e:
+
+        print(e)
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
