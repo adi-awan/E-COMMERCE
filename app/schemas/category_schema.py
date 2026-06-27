a91810a20c0c6b57d9a79083d02cd85b7d79047e
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-06-27 09:49:29 +0500

    upload error

--- app/schemas/category_schema.py
+++ app/schemas/category_schema.py
@@ -4,9 +4,9 @@ from typing import Optional
 
 class CategoryCreate(BaseModel):
     name: str
-    image: Optional[str] = None
+    image_url: Optional[str] = None
 
 
 class CategoryUpdate(BaseModel):
     name: Optional[str] = None
-    image: Optional[str] = None
+    image_url: Optional[str] = None
