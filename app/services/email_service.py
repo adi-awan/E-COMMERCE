1e920dcd4aaf72ecf6589a800653380eafc99de6
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-07-04 11:53:05 +0500

    commit

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,33 +1,27 @@
-from fastapi_mail import FastMail, MessageSchema
-from app.core.email import conf
 import aiosmtplib
 from email.message import EmailMessage
 
-if conf is None:
-    return
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
 
-else:
-    EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_PORT = 587
-
-    EMAIL_USER = "yourgmail@gmail.com"
-    EMAIL_PASSWORD = "your_app_password"
+EMAIL_USER = "yourgmail@gmail.com"
+EMAIL_PASSWORD = "your_app_password"
 
 
-    async def send_email(to_email: str, subject: str, html_content: str):
+async def send_email(to_email: str, subject: str, html_content: str):
 
-        message = EmailMessage()
-        message["From"] = EMAIL_USER
-        message["To"] = to_email
-        message["Subject"] = subject
+    message = EmailMessage()
+    message["From"] = EMAIL_USER
+    message["To"] = to_email
+    message["Subject"] = subject
 
-        message.set_content(html_content, subtype="html")
+    message.set_content(html_content, subtype="html")
 
-        await aiosmtplib.send(
-            message,
-            hostname=EMAIL_HOST,
-            port=EMAIL_PORT,
-            start_tls=True,
-            username=EMAIL_USER,
-            password=EMAIL_PASSWORD,
-        )
+    await aiosmtplib.send(
+        message,
+        hostname=EMAIL_HOST,
+        port=EMAIL_PORT,
+        start_tls=True,
+        username=EMAIL_USER,
+        password=EMAIL_PASSWORD,
+    )
